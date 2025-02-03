66f71e3da1473f409a07df927ef1835fb8b7061d
A S <65203960+asathsara@users.noreply.github.com>
2025-02-03 21:36:46 +0530

    Add entry & submit button

--- app.py
+++ app.py
@@ -5,26 +5,43 @@ class TodoApp:
     def __init__(self, root):
         self.root = root
         self.root.geometry("400x600")
+        self.root.title("Todo App")
 
         self.tasks = []
         self.load_tasks()
 
-        self.container = tk.Frame(root, padx=8, pady=8) # margin
-        self.container.grid(row=0, column=0, sticky="nsew")
+        # Container frame
+        self.container = tk.Frame(root, padx=8, pady=8)  # margin
+        self.container.pack(fill="both", expand=True)
+
+        # Frame for the image and text label to be on the same line
+        self.top_frame = tk.Frame(self.container)
+        self.top_frame.pack(side="top", fill="x")
+
+        self.second_frame = tk.Frame(self.container)
+        self.second_frame.pack(side="top", fill="x")
 
         # Open and resize the image
         image = Image.open("assets/calendar.png")
-        image = image.resize((26, 26))  # Set the desired size
+        image = image.resize((26, 26))
 
         # Convert to Tkinter-compatible format
         self.photo = ImageTk.PhotoImage(image)
 
-        self.image_label = tk.Label(self.container, image=self.photo)
-        self.image_label.grid(row=0, column=0, padx=10, pady=10)
+        # Image label
+        self.image_label = tk.Label(self.top_frame, image=self.photo)
+        self.image_label.pack(side="left", padx=10)
 
-        # Create text label next to the image
-        self.text_label = tk.Label(self.container, text="Task List", font=("Poppins", 16,"bold"))
-        self.text_label.grid(row=0, column=1, pady=10)
+        # Text label next to the image
+        self.text_label = tk.Label(self.top_frame, text="Task List", font=("Poppins", 16, "bold"))
+        self.text_label.pack(side="left", pady=10)
+
+        # Input field (in a new line below)
+        self.input_entry = tk.Entry(self.second_frame, bd=1, font=("Poppins", 12))
+        self.input_entry.pack(side="left", fill="x", padx=10, pady=10,expand=True)
+
+        self.submit_button = tk.Button(self.second_frame, text="Add", font=("Poppins", 10, "bold"), bg="#339fff", fg="white")
+        self.submit_button.pack(side="right", padx=10, pady=10)
 
     def load_tasks(self):
         try:
@@ -32,6 +49,7 @@ class TodoApp:
                 self.tasks = [line.strip() for line in file]  # Python list comprehension
         except FileNotFoundError:
             self.tasks = []
+
 
 # Create the main window
 root = tk.Tk()
